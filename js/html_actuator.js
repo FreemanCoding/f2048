e2e88b04674b23a67a4ff6e4a9801025556b2141
th3walker <t3wschool@gmail.com>
2014-04-03 22:07:21 -0400

    An Update

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "YOU SUCK!";
+  var message = won ? "You win!" : "You Suck!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
